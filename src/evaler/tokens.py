393042151d87af0715a7c2248c3e86f1a7a1d941
SygyzyH <nadavalt12@gmail.com>
2024-10-16 17:55:57 +0300

    Fixed nubmers possibly containing decimal points without additional numbers

--- src/evaler/tokens.py
+++ src/evaler/tokens.py
@@ -191,7 +191,7 @@ class MagnitudeCastHandler(TokenHandler):
 
 BASE_TOKENS = [
     Token('END_STATEMENT', r';', None),
-    Token('NUMBER', r'-?\d+(\.\d*)?j?[' + ''.join(list(KNOWN_MAGNITUDES.keys())) + r']?', NumberHandler),
+    Token('NUMBER', r'-?\d+(\.\d+)?j?[' + ''.join(list(KNOWN_MAGNITUDES.keys())) + r']?', NumberHandler),
     Token('EQUAL', r'=', None, priority=1),
     Token('ADD', r'\+', AdditionHandler, priority=2),
     Token('SUB', r'-', SubtractionHandler, priority=2),
