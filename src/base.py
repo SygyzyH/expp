986ac5e2c757b418f0846121ab689454d0ba72e4
SygyzyH <nadavalt12@gmail.com>
2024-10-17 23:18:50 +0300

    Moved statements constructor to new file

--- src/base.py
+++ src/base.py
@@ -1,6 +1,5 @@
 import tokenizer
 import tokens
-import polish
 import tree
 
 import logging
@@ -9,39 +8,6 @@ import copy
 MAX_SOLUTION_DEPTH = 20
 SOLUTION_EPSIL = 1e-6
 SOLUTION_TOLERANCE = 1e-9
-
-class StatmentConstructor:
-    def __init__(self) -> None:
-        self._polish_inst = polish.PolishConstructor()
-        self._stack = []
-
-    def consume_token(self, token: tokens.Token):
-        for polish_token in self._polish_inst.consume_token(token):
-            logging.debug(f"Polished token {polish_token}")
-            if polish_token.name == 'END_STATEMENT':
-                logging.debug(f"Finished statement {self._stack}")
-                assert len(self._stack) == 1, 'Disjointed expression'
-                # Statement was generated
-                return self._stack.pop()
-            if polish_token.name == 'FUNC_R':
-                logging.debug("Left sided function")
-                assert len(self._stack) > 0, f'line: {polish_token.line}, column: {polish_token.column}: Function missing argument'
-                self._stack.append(tree.BiTree(None, self._stack.pop(), polish_token))
-            elif polish_token.name == 'FUNC_L':
-                logging.debug("Right sided function")
-                assert len(self._stack) > 0, f'line: {polish_token.line}, column: {polish_token.column}: Function missing argument'
-                self._stack.append(tree.BiTree(self._stack.pop(), None, polish_token))
-            elif polish_token.priority > 0:
-                logging.debug("Two sided function")
-                assert len(self._stack) > 1, f'line: {polish_token.line}, column: {polish_token.column}: Operator missing arguments'
-                # NOTE: The flipped order (rhs first in stack, but second in constructor)
-                rhs, lhs = self._stack.pop(), self._stack.pop()
-                self._stack.append(tree.BiTree(lhs, rhs, polish_token))
-            else:
-                logging.debug("Polish takes no arguments")
-                self._stack.append(tree.BiTree(None, None, polish_token))
-        # If no statment was completed, empty yield for now
-        return
 
 def evaluate(exp: tree.BiTree):
     return exp.value.handler.evaluate(exp)
@@ -95,8 +61,9 @@ if __name__ == "__main__":
         level=logging.DEBUG,
         format="[%(levelname)s:%(funcName)s:%(lineno)s] %(message)s"
     )
+    import statement
 
-    statment = StatmentConstructor()
+    statment = statement.StatmentConstructor()
     for token in tokenizer.tokenize("1 + 1 - 1 + 1 + x * x"):
         exp = statment.consume_token(token)
         if exp is not None:
---
+++ src/statement.py
@@ -0,0 +1,36 @@
+import tokens
+import polish
+import tree
+
+class StatmentConstructor:
+    def __init__(self) -> None:
+        self._polish_inst = polish.PolishConstructor()
+        self._stack = []
+
+    def consume_token(self, token: tokens.Token):
+        for polish_token in self._polish_inst.consume_token(token):
+            logging.debug(f"Polished token {polish_token}")
+            if polish_token.name == 'END_STATEMENT':
+                logging.debug(f"Finished statement {self._stack}")
+                assert len(self._stack) == 1, 'Disjointed expression'
+                # Statement was generated
+                return self._stack.pop()
+            if polish_token.name == 'FUNC_R':
+                logging.debug("Left sided function")
+                assert len(self._stack) > 0, f'line: {polish_token.line}, column: {polish_token.column}: Function missing argument'
+                self._stack.append(tree.BiTree(None, self._stack.pop(), polish_token))
+            elif polish_token.name == 'FUNC_L':
+                logging.debug("Right sided function")
+                assert len(self._stack) > 0, f'line: {polish_token.line}, column: {polish_token.column}: Function missing argument'
+                self._stack.append(tree.BiTree(self._stack.pop(), None, polish_token))
+            elif polish_token.priority > 0:
+                logging.debug("Two sided function")
+                assert len(self._stack) > 1, f'line: {polish_token.line}, column: {polish_token.column}: Operator missing arguments'
+                # NOTE: The flipped order (rhs first in stack, but second in constructor)
+                rhs, lhs = self._stack.pop(), self._stack.pop()
+                self._stack.append(tree.BiTree(lhs, rhs, polish_token))
+            else:
+                logging.debug("Polish takes no arguments")
+                self._stack.append(tree.BiTree(None, None, polish_token))
+        # If no statment was completed, empty yield for now
+        return
