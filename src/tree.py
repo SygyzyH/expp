6645ca6e8c27901dd38467b93c637e1969c3a3df
SygyzyH <nadavalt12@gmail.com>
2024-10-17 22:33:50 +0300

    Printable trees

--- src/tree.py
+++ src/tree.py
@@ -5,3 +5,13 @@ class BiTree:
     lhs: "BiTree"
     rhs: "BiTree"
     value: object
+
+    def __repr__(self, depth=1) -> str:
+        tabs = '\t' * depth
+        external_tabs = '\t' * (depth - 1)
+        string = f"{external_tabs}BiTree(\n{tabs}{self.value}\n"
+        if self.lhs is not None:
+            string += f"{tabs}l:\n{self.lhs.__repr__(depth + 1)}\n"
+        if self.rhs is not None:
+            string += f"{tabs}r:\n{self.rhs.__repr__(depth + 1)}\n"
+        return string + external_tabs + ")"
