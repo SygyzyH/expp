2887d8e4ea737647ae93cdcee9ed2d34edd67b10
SygyzyH <nadavalt12@gmail.com>
2024-10-20 21:30:57 +0300

    Polished arguments, added options to supply input and output files

--- src/main.py
+++ src/main.py
@@ -24,6 +24,7 @@ Howto:
 def read_loop():
     print(__doc__)
     # Default directive
+    line_history = []
     expression_history = []
     result_history = []
     variables = {}
@@ -31,12 +32,13 @@ def read_loop():
     while True:
         try:
             line = input(">>> ")
+            line_history.append(line)
 
             for result in line_consumer.consume_line(line, expression_history, result_history, variables, True):
                 pass
         except KeyboardInterrupt:
             logging.info('Quit')
-            raise
+            return '\n'.join(line_history)
         except:
             from traceback import print_exc
             print_exc()
@@ -45,8 +47,9 @@ def read_loop():
 def main():
     parser = argparse.ArgumentParser(prog=__file__, description=__doc__)
 
-    parser.add_argument('--debug', '-d', action=argparse.BooleanOptionalAction)
-    parser.add_argument('--interactive', '-i', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--debug', '-d', nargs='?', type=argparse.FileType('w'), default=None, const=sys.stderr, help='Enables debug logs.')
+    parser.add_argument('--interactive', '-i', nargs='?', type=argparse.FileType('r'), default=None, const=True, help='Interactive mode. If filename is also given, start with the contents of the file loaded.')
+    parser.add_argument('--output-file', '-o', type=str, default=None, help='Path to output file, or "-" for stdout.')
     # TODO: Add input from file, interactive input? (whenever a line updates, re-evaluate it)
     # Maybe also interactive output file? (whenever input at line changes, output at same line changes)
     # Maybe all of this, but in CLI?
@@ -54,15 +57,25 @@ def main():
     args = parser.parse_args()
     
     logging.basicConfig(
-        stream=sys.stderr,
-        level=logging.DEBUG if args.debug else logging.INFO,
+        stream=args.debug if args.debug is not None else sys.stderr,
+        level=logging.DEBUG if args.debug is not None else logging.INFO,
         format="[%(levelname)s:%(funcName)s:%(lineno)s] %(message)s"
     )
 
     if not args.interactive:
-        read_loop()
+        res = read_loop()
     else:
-        textbook.start()
+        input_text = ""
+        if args.interactive is not None:
+            input_text = args.interactive.read()
+        res = textbook.start(input_text)
+    if args.output_file is not None:
+        if args.output_file == '-':
+            print(res)
+        else:
+            with open(args.output_file, 'w') as f:
+                f.write(res)
+
 
 
 if __name__ == "__main__":
--- src/textbook.py
+++ src/textbook.py
@@ -5,13 +5,13 @@ import curses
 import logging
 from traceback import format_exc
 
-def start():
-    curses.wrapper(_start)
+def start(input_file_contents: str):
+    return curses.wrapper(_start, input_file_contents)
 
 class TextContainer:
     ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-=!@#$%^&*()_+[]\\{}|;':\",./<>? \n"
-    def __init__(self) -> None:
-        self._text = [""]
+    def __init__(self, initial_text=[""]) -> None:
+        self._text = initial_text
         self.row = 0
         self.col = 0
 
@@ -72,8 +72,8 @@ class TextContainer:
     def getlines(self):
         return self._text
 
-def _start(stdscr: curses.window):
-    container = TextContainer()
+def _start(stdscr: curses.window, input_file_contents: str):
+    container = TextContainer(input_file_contents.split('\n'))
 
     stdscr.keypad(True)
     curses.noecho()
@@ -93,92 +93,94 @@ def _start(stdscr: curses.window):
 
     char = 0
 
-    while True:
-        if char == curses.KEY_RESIZE:
-            rows, cols = stdscr.getmaxyx()
-            logging.debug(f"updating to {rows} {cols}")
-            curses.resize_term(rows, cols)
-            output_window.resize(rows, cols//2)
-            output_window.mvwin(0, cols//2)
-            input_window.resize(rows, cols//2)
-        elif char == curses.KEY_UP:
-            container.move_up()
-        elif char == curses.KEY_DOWN:
-            container.move_down()
-        elif char == curses.KEY_LEFT:
-            container.move_left()
-        elif char == curses.KEY_RIGHT:
-            container.move_right()
-        elif char == curses.KEY_DC:
-            container.delete_current_char()
-        elif char in (curses.KEY_BACKSPACE, ord('\b')):
-            container.delete_back_char()
-        elif char == curses.KEY_HOME:
-            container.move_start()
-        elif char == curses.KEY_END:
-            container.move_end()
-        elif chr(char) in TextContainer.ALLOWED_CHARS:
-            container.add_char(chr(char))
+    try:
+        while True:
+            if char == curses.KEY_RESIZE:
+                rows, cols = stdscr.getmaxyx()
+                logging.debug(f"updating to {rows} {cols}")
+                curses.resize_term(rows, cols)
+                output_window.resize(rows, cols//2)
+                output_window.mvwin(0, cols//2)
+                input_window.resize(rows, cols//2)
+            elif char == curses.KEY_UP:
+                container.move_up()
+            elif char == curses.KEY_DOWN:
+                container.move_down()
+            elif char == curses.KEY_LEFT:
+                container.move_left()
+            elif char == curses.KEY_RIGHT:
+                container.move_right()
+            elif char == curses.KEY_DC:
+                container.delete_current_char()
+            elif char in (curses.KEY_BACKSPACE, ord('\b')):
+                container.delete_back_char()
+            elif char == curses.KEY_HOME:
+                container.move_start()
+            elif char == curses.KEY_END:
+                container.move_end()
+            elif chr(char) in TextContainer.ALLOWED_CHARS:
+                container.add_char(chr(char))
 
-        input_window.move(0, 0)
-        input_window.clear()
-        irows, icols = input_window.getmaxyx()
-        # Need to account for borders, so actual size is smaller
-        view = (irows - 2) * (container.row // (irows - 2)), (icols - 2) * (container.col // (icols - 2))
-        for i, line in enumerate(container.getbox(*view, irows - 3, icols - 1)):
-            input_window.move(i + 1, 1)
-            input_window.addstr(line)
-        input_window.move(container.row % (irows - 2) + 1, container.col % (icols - 2) + 1)
-        input_window.border()
+            input_window.move(0, 0)
+            input_window.clear()
+            irows, icols = input_window.getmaxyx()
+            # Need to account for borders, so actual size is smaller
+            view = (irows - 2) * (container.row // (irows - 2)), (icols - 2) * (container.col // (icols - 2))
+            for i, line in enumerate(container.getbox(*view, irows - 3, icols - 1)):
+                input_window.move(i + 1, 1)
+                input_window.addstr(line)
+            input_window.move(container.row % (irows - 2) + 1, container.col % (icols - 2) + 1)
+            input_window.border()
 
-        #stdscr.clear()
-        output_window.move(0, 0)
-        output_window.clear()
-        output_window.border()
-        orows, ocols = output_window.getmaxyx()
-        expression_history = []
-        result_history = []
-        variables = {}
-        i = 0
-        for line_number, line in enumerate(container.getlines()[view[0]:view[0] + irows - 2]):
-            output_window.move(i + 1, 1)
-            try:
-                results_count = 0
-                for result in line_consumer.consume_line('\n' * (line_number + view[0]) + line, expression_history, result_history, variables, False):
-                    if result is not None:
-                        results_count += 1
-                        output_window.addnstr(f"{len(result_history)} : {result}", -1)
-                        i += len(result) // (ocols - 1)
-                        output_window.move(i + results_count + 1, 1)
-                i += max(0, results_count - 1)
-            except (syntax_error.SyntaxError, AssertionError) as e:
-                logging.debug(format_exc())
-                if isinstance(e, AssertionError):
-                    e = e.args[0]
-                output_window.addnstr(str(e), -1)
-                i += len(str(e)) // (ocols - 1)
+            #stdscr.clear()
+            output_window.move(0, 0)
+            output_window.clear()
+            output_window.border()
+            orows, ocols = output_window.getmaxyx()
+            expression_history = []
+            result_history = []
+            variables = {}
+            i = 0
+            for line_number, line in enumerate(container.getlines()[view[0]:view[0] + irows - 2]):
+                output_window.move(i + 1, 1)
+                try:
+                    results_count = 0
+                    for result in line_consumer.consume_line('\n' * (line_number + view[0]) + line, expression_history, result_history, variables, False):
+                        if result is not None:
+                            results_count += 1
+                            output_window.addnstr(f"{len(result_history)} : {result}", -1)
+                            i += len(result) // (ocols - 1)
+                            output_window.move(i + results_count + 1, 1)
+                    i += max(0, results_count - 1)
+                except (syntax_error.SyntaxError, AssertionError) as e:
+                    logging.debug(format_exc())
+                    if isinstance(e, AssertionError):
+                        e = e.args[0]
+                    output_window.addnstr(str(e), -1)
+                    i += len(str(e)) // (ocols - 1)
 
-                if e.col + 1 >= view[1] and e.col + 1 < view[1] + icols:
-                    if view[1] > 0:
-                        e.col %= view[1]
-                    last_input_cursor = input_window.getyx()
-                    offending_character = input_window.inch(e.line - view[0], e.col + 1)
-                    input_window.addch(e.line - view[0], e.col + 1, offending_character, curses.color_pair(1))
-                    input_window.move(*last_input_cursor)
-            except Exception as e:
-                logging.debug(format_exc())
-                output_window.addnstr(f"{e.__class__.__name__}: {str(e)}", -1)
-                i += len(f"{e.__class__.__name__}: {str(e)}") // (ocols -1)
-            i += 1
+                    if e.col + 1 >= view[1] and e.col + 1 < view[1] + icols:
+                        if view[1] > 0:
+                            e.col %= view[1]
+                        last_input_cursor = input_window.getyx()
+                        offending_character = input_window.inch(e.line - view[0], e.col + 1)
+                        input_window.addch(e.line - view[0], e.col + 1, offending_character, curses.color_pair(1))
+                        input_window.move(*last_input_cursor)
+                except Exception as e:
+                    logging.debug(format_exc())
+                    output_window.addnstr(f"{e.__class__.__name__}: {str(e)}", -1)
+                    i += len(f"{e.__class__.__name__}: {str(e)}") // (ocols -1)
+                i += 1
 
-        input_window.noutrefresh()
-        output_window.noutrefresh()
-        
-        stdscr.move(*input_window.getyx())
-        stdscr.refresh()
+            input_window.noutrefresh()
+            output_window.noutrefresh()
+            
+            stdscr.move(*input_window.getyx())
+            stdscr.refresh()
 
-        char = stdscr.getch()
-        
+            char = stdscr.getch()
+    except KeyboardInterrupt:
+        return '\n'.join(container.getlines())
 
 if __name__ == "__main__":
     start()
